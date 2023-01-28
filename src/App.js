caf39b274abb6a44c3a9f4d6ac16f1552e31e4b9
Mineralka <Chdv962007@gmail.com>
2023-01-28 18:22:27 +0300

    FtoCfix

--- src/App.js
+++ src/App.js
@@ -7,49 +7,49 @@ import { useState } from 'react';
 // import Footer from './Footer';
 
 function App() {
-  // const [degreeF, setDegreeF] = useState(0)
-  // function handleChange1(event){
-  //   setDegreeF(+event.target.value);
-  // }
-
-
-  const [input1, setInput1] = useState(0)
-  const [input2, setInput2] = useState(0)
-  const [input3, setInput3] = useState(0)
-  const [input4, setInput4] = useState(0)
-  const [input5, setInput5] = useState(0)
+  const [degreeF, setDegreeF] = useState(0)
   function handleChange1(event){
-    setInput1(+event.target.value);
+    setDegreeF(+event.target.value);
   }
-  function handleChange2(event){
-    setInput2(+event.target.value);
-  }
-  function handleChange3(event){
-    setInput3(+event.target.value);
-  }
-  function handleChange4(event){
-    setInput4(+event.target.value);
-  }
-  function handleChange5(event){
-    setInput5(+event.target.value);
-  }
-  let sum = input1+input2+input3+input4+input5
+
+
+  // const [input1, setInput1] = useState(0)
+  // const [input2, setInput2] = useState(0)
+  // const [input3, setInput3] = useState(0)
+  // const [input4, setInput4] = useState(0)
+  // const [input5, setInput5] = useState(0)
+  // function handleChange1(event){
+  //   setInput1(+event.target.value);
+  // }
+  // function handleChange2(event){
+  //   setInput2(+event.target.value);
+  // }
+  // function handleChange3(event){
+  //   setInput3(+event.target.value);
+  // }
+  // function handleChange4(event){
+  //   setInput4(+event.target.value);
+  // }
+  // function handleChange5(event){
+  //   setInput5(+event.target.value);
+  // }
+  // let sum = input1+input2+input3+input4+input5
   return (
-  // <div>
-  //   <span>F° = </span>
-  //   <input value={degreeF} onInput={handleChange1}/><br/>
-  //   <span>C° = {degreeF / 33.8}</span>
-  // </div>
-
-
   <div>
-    <input value={input1} onInput={handleChange1}/><br/>
-    <input value={input2} onInput={handleChange2}/><br/>
-    <input value={input3} onInput={handleChange3}/><br/>
-    <input value={input4} onInput={handleChange4}/><br/>
-    <input value={input5} onInput={handleChange5}/><br/>
-    <span>Среднее арифметическое = {sum / 5}</span>
+    <span>F° = </span>
+    <input value={degreeF} onInput={handleChange1}/><br/>
+    <span>C° = {5/9*(degreeF - 32)}</span>
   </div>
+
+
+  // <div>
+  //   <input value={input1} onInput={handleChange1}/><br/>
+  //   <input value={input2} onInput={handleChange2}/><br/>
+  //   <input value={input3} onInput={handleChange3}/><br/>
+  //   <input value={input4} onInput={handleChange4}/><br/>
+  //   <input value={input5} onInput={handleChange5}/><br/>
+  //   <span>Среднее арифметическое = {sum / 5}</span>
+  // </div>
   );
 }
 // function App() {
